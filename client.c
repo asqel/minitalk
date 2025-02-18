cfdb47998b0a894bea2cd7aa5689000ae6e2bff7
asqel <axel.lleres@gmail.com>
2025-02-18 20:30:50 +0100

    update

--- client.c
+++ client.c
@@ -1,27 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axlleres <axlleres@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 20:16:08 by axlleres          #+#    #+#             */
+/*   Updated: 2025/02/18 20:28:35 by axlleres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
-void write_error(char *error) {
-	int len;
+int	write_error(char *error)
+{
+	int	len;
 
 	len = 0;
 	while (error[len])
 		len++;
 	write(2, error, len);
+	return (1);
 }
 
-int ft_atoi(char *str) {
-	int sign;
-	int res;
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	res;
 
 	sign = 1;
 	res = 0;
 	if (*str == '-')
-		return -1;
-	while (*str >= '0' && *str <= '9' && *str != '\0') {
+		return (-1);
+	while (*str >= '0' && *str <= '9' && *str != '\0')
+	{
 		res *= 10;
 		res += *str - '0';
 		str++;
@@ -29,33 +45,51 @@ int ft_atoi(char *str) {
 	return (res * sign);
 }
 
-void send_char(char c, int pid) {
-	int i;
+void	send_char(char c, int pid)
+{
+	int	i;
 
 	i = 7;
-	while (i >= 0) {
+	while (i >= 0)
+	{
 		if ((c >> i) & 1)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
 		i--;
-		usleep(10000);
+		usleep(500);
 	}
 }
 
-int main(int argc, char **argv) {
-	if (argc != 3) {
-		write_error("Wrong number of arguments\nUsage: <PID> <message>\n");
-		return (1);
-	}
-	int pid = ft_atoi(argv[1]);
-	char *message = argv[2];
+void	sig_handler(int signo)
+{
+	(void)signo;
+	write(1, "Server received our message\n", 28);
+	exit(0);
+}
 
-	int i = 0;
-	while (message[i]) {
+int	main(int argc, char **argv)
+{
+	int		pid;
+	int		i;
+	char	*message;
+
+	signal(SIGUSR1, sig_handler);
+	if (argc != 3)
+		return (write_error("Wrong arguments\nUsage: <PID> <message>\n"));
+	pid = ft_atoi(argv[1]);
+	if (pid == -1)
+		return (write_error("Wrong PID\n"));
+	message = argv[2];
+	i = 0;
+	while (message[i])
+	{
 		send_char(message[i], pid);
 		i++;
 	}
+	if (i <= 0 || message[i - 1] != '\n')
+		send_char('\n', pid);
+	send_char('\0', pid);
+	pause();
 	return (0);
-
 }
--- server.c
+++ server.c
@@ -1,23 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: axlleres <axlleres@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 20:20:02 by axlleres          #+#    #+#             */
+/*   Updated: 2025/02/18 20:27:44 by axlleres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
 
-char msg_info[2] = {0, 0};
+int	g_msg_info[3] = {0, 0, 0};
 
-void sig_handler(int signo) {
+void	sig_handler(int signo, siginfo_t *info, void *context)
+{
+	(void)context;
 	if (signo == SIGUSR1)
 	{
-		msg_info[0] <<= 1;
-		msg_info[0] |= 1;
+		g_msg_info[0] <<= 1;
+		g_msg_info[0] |= 1;
 	}
 	else if (signo == SIGUSR2)
 	{
-		msg_info[0] <<= 1;
+		g_msg_info[0] <<= 1;
 	}
-	msg_info[1]++;
+	g_msg_info[1]++;
+	g_msg_info[2] = info->si_pid;
 }
 
-void ft_putnbr(int nb) {
+void	ft_putnbr(int nb)
+{
 	if (nb == -2147483648)
 		write(1, "-2147483648", 11);
 	else if (nb < 0)
@@ -30,21 +46,37 @@ void ft_putnbr(int nb) {
 	}
 }
 
-int main() {
-	signal(SIGUSR1, sig_handler);
-	signal(SIGUSR2, sig_handler);
+void	send_acknowledgement(void)
+{
+	if (g_msg_info[2] != 0)
+		kill(g_msg_info[2], SIGUSR1);
+}
 
-	int pid = getpid();
+int	main(void)
+{
+	struct sigaction	sa;
+	int					pid;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = sig_handler;
+	sigemptyset(&sa.sa_mask);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1
+		|| sigaction(SIGUSR2, &sa, NULL) == -1)
+		return (write(1, "Error\n", 6), 1);
+	pid = getpid();
 	write(1, "Server PID: ", 12);
 	ft_putnbr(pid);
 	write(1, "\n", 1);
 	while (1)
 	{
-		if (msg_info[1] == 8)
+		if (g_msg_info[1] == 8)
 		{
-			write(1, &msg_info[0], 2);
-			msg_info[0] = 0;
-			msg_info[1] = 0;
+			if (g_msg_info[0] == 0)
+				send_acknowledgement();
+			write(1, &g_msg_info[0], 1);
+			g_msg_info[0] = 0;
+			g_msg_info[1] = 0;
 		}
+		pause();
 	}
 }
